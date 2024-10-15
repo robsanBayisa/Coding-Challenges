19a3215c8f49ea24811aa942fa99ffdd94a91d97
Robsan Bayisa <robsanbayisa@gmail.com>
2024-10-15 05:12:48 +0300

    added some

--- CC-02-12/app.js
+++ CC-02-12/app.js
@@ -1,5 +1,5 @@
 howManyYears = (date1, date2) => {
   const value =
     parseInt(date2.split('/').shift()) - parseInt(date1.split('/').shift());
-  return value < 0 ?  -value :  value;
+  return value < 0 ?  -value : value;
 };
